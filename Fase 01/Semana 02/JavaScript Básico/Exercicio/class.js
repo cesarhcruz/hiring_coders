af213db0bba0aeccd68467c76ac8c4ef23c1284a
cesarhcruz <cesar.hcruz88@gmail.com>
2022-05-12 22:00:06 -0300

    adicionado conteudos novos

---
+++ Fase 01/Semana 02/JavaScript Básico/Exercicio/class.js
@@ -0,0 +1,16 @@
+class Matematica{
+
+    soma(valorA, valorB){
+        return valorA + valorB;
+    }
+
+    subtracao(valorA, valorB){
+        return valorA - valorB;
+    }
+}
+
+var instanciaMatematica = new Matematica();
+
+var resultado = instanciaMatematica.soma(4, 7);
+
+console.log(resultado)
---
+++ Fase 01/Semana 02/JavaScript Básico/Exercicio/operadores.js
@@ -0,0 +1,11 @@
+var operador = prompt("Digite um valor");
+var operando = 10;
+
+var resultado = operador * operando;
+
+if (resultado > 1000){
+    console.log("valor digitado é muito alto");
+} else {
+    console.log("o resultado é: " + resultado);
+}
+
